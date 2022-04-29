35ea59123ce58be1ade2ef3e3f1bd36615d431ba
Irving Rodríguez <irvingrodriguezromero@gmal.com>
2022-04-28 21:43:06 -0500

    Creating files structure and styles of Form component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import Calendar from './components/Calendar/Calendar';
+import Form from './components/Form/Form';
+import { BiMoon } from "react-icons/bi";
 
 function App() {
+  let image = "url(https://cdn.pixabay.com/photo/2021/09/02/16/48/cat-6593947_960_720.jpg)";
+  
   return (
     <div>
-      <Calendar/>
+      <Form title="Create a new customer" variant="flex" labelButton="Create account" size="small" text="Here you can add the information to create and register him in your database" backgroundImage="https://www.thestreet.com/.image/ar_16:9%2Cc_fill%2Ccs_srgb%2Cg_faces:center%2Cq_auto:good%2Cw_768/MTY3NTM5MzU5MDg3MjczODcw/business-structure-which-type-is-best-for-your-business.png">
+        <div className="input-container">
+                <div className="input-content">
+                    <input type="email" className="input-content-control" placeholder="Email address" autoComplete="off" />
+                    <BiMoon/>
+                </div>
+                <span className="input-content-legend">Invalid value</span>
+            </div>
+            <div className="input-container">
+                <div className="input-content">
+                    <input type="password" className="input-content-control" placeholder="Password" autoComplete="off" />
+                    <BiMoon/>
+                </div>
+                <span className="input-content-legend">Invalid value</span>
+            </div>
+            <div className="input-container">
+                <div className="input-content">
+                    <input type="password" className="input-content-control" placeholder="Confirm password" autoComplete="off" />
+                    <BiMoon/>
+                </div>
+                <span className="input-content-legend">Invalid value</span>
+            </div>
+      </Form>
     </div>
   );
 }
--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,10 +1,61 @@
+import PropTypes from 'prop-types';
+import "../../css/form.css";
 
-const Form = () => {
+const Form = ({ variant, title, size, labelButton, alert, shadow, dark, backgroundImage, gradientColor, className, borderRadius, backgroundColor, text, children}) => {
+
+    const backgroundImageStyles = {
+            background: `${gradientColor ? `linear-gradient(${gradientColor}, ${gradientColor})` : 'linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.5))'}, url(${backgroundImage && backgroundImage})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            backgroundPosition: "center",
+            backgroundColor: backgroundColor
+        };
+
   return (
-    <form>
-      
+    <form className={`form form-size--${size} ${dark && 'dark'} ${shadow && shadow} ${borderRadius && borderRadius} ${className && className} ${backgroundImage && 'backgroundImage'}`} style={(backgroundImage && backgroundImageStyles) || (backgroundColor && backgroundImageStyles)}>
+      <h2 className="form-title">{title ? title : 'Login'}</h2>
+      <p className="form-text">{text && text}</p>
+      <div className={`form-inputs-container form-variant--${variant}`}>
+          {children}
+      </div>
+      <div className="form-footer">
+         <button className="form-button">{labelButton ? labelButton : 'Ok'}</button>
+      </div>
+      {alert && <div className="form-alert ">{alert}</div>}
     </form>
-  )
+  );
 }
 
 export default Form;
+
+Form.propTypes = {
+    variant: PropTypes.oneOf(['flex', 'grid', 'default']),
+    title: PropTypes.string,
+    size: PropTypes.oneOf(['small', 'medium', 'large', 'default']),
+    labelButton: PropTypes.string,
+    alert: PropTypes.string,
+    shadow: PropTypes.bool,
+    borderRadius: PropTypes.bool,
+    dark: PropTypes.bool,
+    backgroundImage: PropTypes.string,
+    gradientColor: PropTypes.string,
+    backgroundColor: PropTypes.string,
+    text: PropTypes.string,
+    onSubmit: PropTypes.func
+  };
+
+  Form.defaultProps = {
+    variant: 'flex',
+    title: '',
+    size: 'default',
+    labelButton: '',
+    text: '',
+    alert: null,
+    shadow: true,
+    borderRadius: false,
+    dark: false,
+    backgroundImage: null,
+    gradientColor: null,
+    backgroundColor: null,
+    onSubmit: undefined
+  };

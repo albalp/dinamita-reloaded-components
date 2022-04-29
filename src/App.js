cbeed1a7a2ecf9e12a24873836b720cb813bdb98
Irving Rodríguez <irvingrodriguezromero@gmal.com>
2022-04-29 09:20:57 -0500

    Removing html and styles unnecessary

--- src/App.js
+++ src/App.js
@@ -6,28 +6,8 @@ function App() {
   return (
 
     <Form>
-        <div className="input-container">
-                <div className="input-content">
-                    <input type="email" className="input-content-control" placeholder="Email address" autoComplete="off" />
-                    <BiMoon/>
-                </div>
-                <span className="input-content-legend">Invalid value</span>
-            </div>
-            <div className="input-container">
-                <div className="input-content">
-                    <input type="password" className="input-content-control" placeholder="Password" autoComplete="off" />
-                    <BiMoon/>
-                </div>
-                <span className="input-content-legend">Invalid value</span>
-            </div>
-            <div className="input-container">
-                <div className="input-content">
-                    <input type="password" className="input-content-control" placeholder="Confirm password" autoComplete="off" />
-                    <BiMoon/>
-                </div>
-                <span className="input-content-legend">Invalid value</span>
-            </div>
-      </Form>
+        
+    </Form>
   );
 }
 

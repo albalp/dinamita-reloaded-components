b1aee9392d5c6c12dac39ea701f6032547557094
Irving Rodríguez <irvingrodriguezromero@gmal.com>
2022-05-03 14:28:34 -0500

    Creación rama de integracion

--- src/App.js
+++ src/App.js
@@ -1,29 +1,34 @@
-<<<<<<< HEAD
 import {useState} from 'react';
 import { BiUser } from "react-icons/bi";
 import Form from './components/Form/Form/Form';
 import Input from './components/Form/Input/Input';
 import Checkbox from './components/Form/Checkbox/Checkbox';
-=======
 import AOS from "aos";
-import SaveButton from './components/saveButton';
->>>>>>> e10a4530df6ebccc244820c6bc7706b64b660ca8
-
+import SaveButton from './components/SaveButton';
 //import DropdownOpt from './components/dropdown/DropdownOpt';
-import Dropdown from './components/dropdown/Dropdown';
+import Dropdown from './components/dropdown/DropDown';
 import data from '../src/api/mocks.json';
-
 import { MdArrowDropDown } from 'react-icons/md'
 import AddTaskIcon from '@mui/icons-material/AddTask';
-
 import Header from './components/header/header';
 import TextArea from './components/textArea/textArea';
-
 import Modals from './components/modal/modals';
 
-
 function App() {
-<<<<<<< HEAD
+
+  function Clicked(){
+    console.log("Clicked!");
+  }
+
+  const projectData = [
+     {
+       id:data.project.ProjectId, 
+       name:data.project.ProjectName,
+     }
+  ]
+  
+  const categories = []
+  data.project.ProjectCategories.map(category => categories.push({id:category.CategoryID, name:category.CategoryName}))
 
   const initialValues = {
     name: {value: '', valid: null},
@@ -57,37 +62,6 @@ function App() {
 
   return (
 
-    <Form  variant="flex" alert="Ops, ocurred it an error" valid={true}>
-      <Input name="name" onChange={inputsValidations} placeholder="Name" legend="Invalid name" value={initialForm.name.value} valid={initialForm.name.valid}/>
-      <Input name="lastname" onChange={inputsValidations} placeholder="Lastname" legend="Invalid lastname" icon={<BiUser/>} value={initialForm.lastname.value} valid={initialForm.lastname.valid}/>
-      <Input name="email" onChange={inputsValidations} type="email" label="Email address" legend="Invalid email" value={initialForm.email.value} valid={initialForm.email.valid}/>
-      <Input name="password" onChange={inputsValidations} type="password" legend="Invalid password" secondary secondaryPlaceholder="Password" value={initialForm.password.value} valid={initialForm.password.valid}/>
-      <Checkbox label="Male" value="male" secondary />
-      <Checkbox label="Female" value="female" />
-      <Checkbox label="Unknown" value="unknown"  tertiary/>
-      <Checkbox label="Other" value="other" rounded/>
-    </Form>
-  );
-}
-
-export default App;
-=======
-  function Clicked(){
-    console.log("Clicked!");
-  }
-
-  const projectData = [
-     {
-       id:data.project.ProjectId, 
-       name:data.project.ProjectName,
-     }
-  ]
-  const categories = []
-  data.project.ProjectCategories.map(category => categories.push({id:category.CategoryID, name:category.CategoryName}))
-
-
-
-  return (
     <div>
       <Dropdown title="Project" icon={<MdArrowDropDown />} options={projectData}/>
       <Dropdown title="Categories" icon={<MdArrowDropDown />} options={categories}/>
@@ -95,6 +69,17 @@ export default App;
       <TextArea />
       <SaveButton variant="success" label="Guardar" borderRadius icon={<AddTaskIcon/>} onClick={Clicked} />
       <Modals />
+
+      <Form  variant="flex" alert="Ops, ocurred it an error" valid={true}>
+        <Input name="name" onChange={inputsValidations} placeholder="Name" legend="Invalid name" value={initialForm.name.value} valid={initialForm.name.valid}/>
+        <Input name="lastname" onChange={inputsValidations} placeholder="Lastname" legend="Invalid lastname" icon={<BiUser/>} value={initialForm.lastname.value} valid={initialForm.lastname.valid}/>
+        <Input name="email" onChange={inputsValidations} type="email" label="Email address" legend="Invalid email" value={initialForm.email.value} valid={initialForm.email.valid}/>
+        <Input name="password" onChange={inputsValidations} type="password" legend="Invalid password" secondary secondaryPlaceholder="Password" value={initialForm.password.value} valid={initialForm.password.valid}/>
+        <Checkbox label="Male" value="male" secondary />
+        <Checkbox label="Female" value="female" />
+        <Checkbox label="Unknown" value="unknown"  tertiary/>
+        <Checkbox label="Other" value="other" rounded/>
+      </Form>
     </div>
   );
 }
@@ -102,4 +87,3 @@ export default App;
 export default App;
 
 AOS.init();
->>>>>>> e10a4530df6ebccc244820c6bc7706b64b660ca8

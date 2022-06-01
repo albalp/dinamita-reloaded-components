c0ab1e6ec6d22d499f83d4b552a0ee7c8db6022b
AlbaTB <73853188+albalp@users.noreply.github.com>
2022-06-01 11:05:49 -0500

    Webpack working and rendering

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,73 @@
-
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const path =require('path'); //Método de node para resolver la ruta absoluta
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const FaviconsWebpackPlugin = require("favicons-webpack-plugin");
+const webpack = require("webpack");
+const { ModuleFederationPlugin } = webpack.container;
+const deps = require("./package.json").dependencies;
 module.exports = {
-    entry: './src/index.js', 
-    output: {
-        path: path.resolve(__dirname, 'build') //Este output es porque queremos que la carpeta por default de salida que tiene el nombre de dist se cambie al que aquí especifiqumos usando métodos de node
-    },
-    resolve: {
-      extensions: [".js", ".jsx"],
+  entry: "./src/index.js",
+  output: {
+    publicPath: "http://localhost:3001/",
   },
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                loader: 'babel-loader',
-                options: {
-                    presets: [
-                        [
-                            '@babel/preset-react',
-                            {
-                                runtime: 'automatic' // This is the configuration that allows you not to import React from 'react' It's a good practice
-                            }
-                        ]
-                    ]
-                }
-            },
-            {
-                test: /\.css$/,
-                use: ['style-loader', 'css-loader']
-            }
-        ]
-    },
-    plugins: [ //This is an array, because we can have multiple plugins
-        new HtmlWebpackPlugin({template: 'public/index.html'})
-    ]
-        
-}
+
+  devServer: {
+    open: true,
+    port: 3001,
+    historyApiFallback: true,
+  },
+
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        loader: "babel-loader",
+        options: {
+          presets: [
+            [
+              "@babel/preset-react",
+              {
+                runtime: "automatic", // This is the configuration that allows you not to import React from 'react' It's a good practice
+              },
+            ],
+          ],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    // new webpack.DefinePlugin({
+    // 'process.env': JSON.stringify(process.env),
+    // }),
+    new FaviconsWebpackPlugin("./public/favicon.ico"),
+    new ModuleFederationPlugin({
+      name: "modal",
+      filename: "remoteEntry.js",
+      remotes: {},
+      exposes: {
+        "./modal": "./src/App.js",
+      },
+
+      shared: {
+        ...deps,
+
+        react: { singleton: true, eager: true, requiredVersion: deps.react },
+
+        "react-dom": {
+          singleton: true,
+          eager: true,
+          requiredVersion: deps["react-dom"],
+        },
+      },
+    }),
+
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+};

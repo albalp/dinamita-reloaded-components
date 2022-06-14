7c9d32c049c047ba034db3fa12df7cfdb73e28ef
AlbaTB <73853188+albalp@users.noreply.github.com>
2022-06-14 17:36:47 -0500

    Connection between angular button and open the modal

--- src/components/modal/useModal.jsx
+++ src/components/modal/useModal.jsx
@@ -1,12 +1,16 @@
 //hook
 import {useState} from 'react';
+import { Observable } from 'windowed-observable';
 
 export const useModal = (initialValue) => {
+  const observable = new Observable('modal-state');
+  
   const [isOpen, setIsOpen] = useState(initialValue);
 
   const openModal = (e) => setIsOpen(true);
   const closeModal = (e) => setIsOpen(false);
-
+  observable.subscribe(openModal)
   //variable de estado, método que abre, método que cierra
   return {isOpen, openModal, closeModal}; 
+
 };
